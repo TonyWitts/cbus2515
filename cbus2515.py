435ff23d0a8d7d5a571670a9c9f3174077d4123f
TonyWitts <80721515+TonyWitts@users.noreply.github.com>
2022-10-18 13:58:09 +0100

    Add files via upload

--- cbus2515.py
+++ cbus2515.py
@@ -1,6 +1,7 @@
 # MicroPython CBUS/MCP2515 CAN controller driver
 # (c)2021 Tony Witts
 
+# 221018 - Send handles Msg of None.
 # 220604 - First commit to GitHub.
 # 210322 - Handles Enumeration in and out
 #        - debug parameter to print debug messages
